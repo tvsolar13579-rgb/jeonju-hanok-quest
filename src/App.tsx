0731e438cd7aa472fdfb79fe9ee1e487e03d3cd5
tvsolar13579-rgb <tvsolar13579@gmail.com>
2026-07-14 08:57:11 +0900

    Add admin routes for login and dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import Home from './pages/Home'
 import Region from './pages/Region'
+import AdminLogin from './pages/AdminLogin'
+import AdminDashboard from './pages/AdminDashboard'
 
 const queryClient = new QueryClient()
 
@@ -11,6 +13,8 @@ export default function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/admin/login" element={<AdminLogin />} />
+          <Route path="/admin" element={<AdminDashboard />} />
           <Route path="/:slug" element={<Region />} />
         </Routes>
       </BrowserRouter>
